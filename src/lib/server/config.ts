7fef7f51415069746ecca210c90f2e60caa81a80
khushal saboo <khushalsaboo108@gmail.com>
2026-02-23 13:26:34 +0530

    update the working apis in config.ts

--- src/lib/server/config.ts
+++ src/lib/server/config.ts
@@ -86,12 +86,13 @@ export const API_ENDPOINTS = {
     GET: "/holiday/get",
     SINGLE: "/holiday/single",
     DELETE: (id: number) => `/holiday/${id}`,
-    WORKING_RULES: {
-      BASE: "/working-rule/get",
-      CREATE: "/working-rule/create",
-      SINGLE: "/working-rule/single",
-      UPDATE: (id: string) => `/working-rule/${id}`,
-      DELETE: (id: string) => `/working-rule/${id}`,
-    },
   },
+  WORKING_RULES: {
+    BASE: "/working-rule/get",
+    CREATE: "/working-rule/create",
+    SINGLE: "/working-rule/single",
+    UPDATE: (id: string) => `/working-rule/${id}`,
+    DELETE: (id: string) => `/working-rule/${id}`,
+  },
+  
 } as const;
